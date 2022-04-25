fcb2c75b52c9213f6a1bbbcb0a0f9334301dd0fc
karlrdev <30607101+karlrdev@users.noreply.github.com>
2022-04-25 06:49:44 +0100

    Init

---
+++ FormExample/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using FormExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace FormExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new LoginViewModel { EducationList = GetList(), UserEducation = new List<Education>
+            {
+                new Education()
+            } });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        private List<SelectListItem> GetList()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "Art",
+                    Value = "0"
+                },
+                new SelectListItem
+                {
+                    Text = "Music",
+                    Value = "1"
+                },
+                new SelectListItem
+                {
+                    Text = "English",
+                    Value = "2"
+                },
+                new SelectListItem
+                {
+                    Text = "Math",
+                    Value = "3"
+                }
+            };
+        }
+
+        public IActionResult GetNewRow(int index)
+        {
+            var list = new List<Education>();
+
+            for (int i = 0; i < index + 1; i++)
+            {
+                list.Add(new Education());
+            }
+
+
+            return PartialView("~/Views/Shared/dataRow.cshtml", new AddRowViewModel()
+            {
+                EducationList = GetList(),
+                UserEducation = list,
+                Index = index
+            });
+        }
+
+        public IActionResult FormSubmit(LoginViewModel model)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FormExample/Models/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormExample.Models
+{
+    public class AddRowViewModel
+    {
+        public int Index { get; set; }
+        public List<SelectListItem> EducationList { get; set; }
+
+        public List<Education> UserEducation { get; set; }
+    }
+
+    public class Education
+    {
+        public string Name { get; set; }
+        public string Grade { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        public List<SelectListItem> EducationList { get; set; }
+
+        public List<Education> UserEducation { get; set; }
+    }
+}
